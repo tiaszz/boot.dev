99723c902d21e8abc353de85ee4099722b435fb6
tiaszz <iskiizeh3@gmail.com>
2024-07-17 16:39:14 -0300

    overlap assignment. boot.dev

--- python-poo/polymorphism/check-overlap/main.py
+++ python-poo/polymorphism/check-overlap/main.py
@@ -1,6 +1,12 @@
 class Rectangle:
     def overlaps(self, rect):
-        pass
+        if (self.get_left_x() <= rect.get_right_x() and
+            self.get_right_x() >= rect.get_left_x() and
+            self.get_top_y() >= rect.get_bottom_y() and
+            self.get_bottom_y() <= rect.get_top_y()):
+            return True
+        else:
+            return False
 
     # don't touch below this line
 
