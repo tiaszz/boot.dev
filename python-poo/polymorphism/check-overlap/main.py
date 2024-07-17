68a385ac01e6789cc4ddc2c59658c223d591b0d3
tiaszz <iskiizeh3@gmail.com>
2024-07-17 11:56:21 -0300

    More assignment from boot.dev oop polymorphism

---
+++ python-poo/polymorphism/check-overlap/main.py
@@ -0,0 +1,35 @@
+class Rectangle:
+    def overlaps(self, rect):
+        pass
+
+    # don't touch below this line
+
+    def __init__(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+
+    def get_left_x(self):
+        if self.__x1 < self.__x2:
+            return self.__x1
+        return self.__x2
+
+    def get_right_x(self):
+        if self.__x1 > self.__x2:
+            return self.__x1
+        return self.__x2
+
+    def get_top_y(self):
+        if self.__y1 > self.__y2:
+            return self.__y1
+        return self.__y2
+
+    def get_bottom_y(self):
+        if self.__y1 < self.__y2:
+            return self.__y1
+        return self.__y2
+
+    def __repr__(self):
+        return f"Rectangle({self.__x1}, {self.__y1}, {self.__x2}, {self.__y2})"
+
