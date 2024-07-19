aa08733d2481abaf0f1176435d08cdf0a7f96f1b
tiaszz <iskiizeh3@gmail.com>
2024-07-18 23:00:36 -0300

    finishing polymorphism

---
+++ python-poo/polymorphism/overloading-bult-in/main.py
@@ -0,0 +1,8 @@
+class Dragon:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def __str__(self):
+        return f"I am {self.name}, the {self.color} dragon"
+
