8f08292d1be26179de3d49933279ccbeb5faca6c
tiaszz <iskiizeh3@gmail.com>
2024-09-26 12:19:52 -0300

    challenge adding markdown image programmatically

--- functional/markdown-image/main.py
+++ functional/markdown-image/main.py
@@ -1,3 +1,15 @@
 def create_markdown_image(alt_text):
-    pass
+    markdown_image = f"![{alt_text}]"
 
+    def inner_function(url):
+        escaped_url = url.replace("(", "%28").replace(")", "%29")
+        markdown_image_url = f"{markdown_image}({escaped_url})"
+
+        def innermost_function(title=None):
+            if title:
+                return f'{markdown_image_url[:-1]} "{title}")'
+            return markdown_image_url
+
+        return innermost_function
+
+    return inner_function
